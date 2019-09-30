a656d75fb3bdfefac9f412cf359fe8a8e315dcd1
Rodolfo Hernandez <rodolfoh94@gmail.com>
2019-09-29 17:16:57 -0700

    ParametrosMiercoles

--- Programas/PasoParametros/Program.cs
+++ Programas/PasoParametros/Program.cs
@@ -27,23 +27,55 @@ namespace Prueba
     {
         p = new Persona ();
         p.nombre= "New";
+    
     }
+
+   
+
+
+
 }
 
     class Program
     
     {
+    static void duplica(in int x) // in es estricto no se puede modificar, se pasa por referencia se vigila que no se modifique
+    {
+        x= x*2;
+    }
+
+    static int suma(int x, int y) //ref se puede inicializar o no
+    {
+        return x*y;
+    }
+
+    static void suma(int x, int y,out int res) // out debe de pasarse las variables sin iniciarlizar nosotros lo debemos de hacer
+    {
+        res= x+y;
+    }
+
         static void Main(string[] args)
         {
            
-           Persona p= new Persona();
-           Console.WriteLine(p.nombre);
-           Anonymous.Anonimiza(p);
-           Anonymous.cambia(ref p);
-           Console.WriteLine(p.nombre); 
+           int a =2;
+            int b =3;
+            int r= suma(a,b);
+            int r2;
+            suma(a,b,out r2);
+           Console.WriteLine(r2);
+
+
+
+
+           //duplica(ref a);
+           
+           //Persona p= new Persona();
+          // Console.WriteLine(p.nombre);
+          // Anonymous.Anonimiza(p);
+          // Anonymous.cambia(ref p);
+          // Console.WriteLine(p.nombre); 
 
            
         }
     }
 }
-
