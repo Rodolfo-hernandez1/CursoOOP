95358fb14998e69219feb071c74aaf8a99e04802
Rodolfo Hernandez <rodolfoh94@gmail.com>
2019-09-29 17:09:00 -0700

    PasoParametros

---
+++ Programas/PasoParametros/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practica
+{
+
+    class Duplicador
+    {
+        public void duplica(int x){
+            x= x*2;
+        }
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
