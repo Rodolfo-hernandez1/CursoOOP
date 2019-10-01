c826d256d7709c03d4981aa5cb7cb36e8ca31227
Rodolfo Hernandez <rodolfoh94@gmail.com>
2019-09-30 20:10:37 -0700

    EjemploClaseHoy

---
+++ Programas/EjemploThis/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EjemploThiss
+{
+
+    class Componente
+    {
+            public string nombre;
+            public Contenedor padre;
+
+            public Componente(string nombre, Contenedor padre)
+            {
+                this.nombre = nombre;
+                this.padre = padre;
+
+            }
+
+            public void print()
+            {
+                Console.WriteLine($"Soy {nombre} y mi padre es {padre.nombre}");
+            }
+
+
+    }
+
+
+    class Contenedor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
