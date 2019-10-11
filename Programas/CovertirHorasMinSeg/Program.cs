eb482774a0d0b4b98a2d567bf884218418234888
Rodolfo Hernandez <rodolfoh94@gmail.com>
2019-10-10 22:42:55 -0700

    EjercicioConvertirParte1

---
+++ Programas/CovertirHorasMinSeg/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PracticaDuracion
+{
+    class Duracion
+    {
+        private int Horas;
+        private int Minutos;
+        private int Segundos;
+
+    public Duracion(int h, int m,int s)
+    {
+        Horas=h;
+        Minutos=m;
+        Segundos=s;
+    }
+
+    public void Imprime()
+    {
+        Console.WriteLine("Horas:{0},Minutos:{1},Segundos:{2}",Horas,Minutos,Segundos);
+    }
+
+
+
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duracion Horas= new Duracion(2,0,0);
+            Duracion Minutos= new Duracion(0,10,0);
+            Duracion Segundos= new Duracion(0,0,20);
+
+            Horas.Imprime();
+            Minutos.Imprime();
+            Segundos.Imprime();
+        }
+    }
+}
+
