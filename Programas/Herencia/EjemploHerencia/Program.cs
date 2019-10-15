f0f39c38030cfc7549b4e68b57f761b38942b6a4
Rodolfo Hernandez <rodolfoh94@gmail.com>
2019-10-14 20:22:43 -0700

    HerenciaEjemplo

---
+++ Programas/Herencia/EjemploHerencia/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EjemploHerencia
+{
+    class Musico
+    {
+        protected string nombre;
+
+        public Musico(string n)
+        {
+            nombre = n;
+        }
+
+        public void saluda()
+        {
+            Console.WriteLine("Hola Soy {0}",nombre);
+        }
+
+        public void afinar()
+        {
+            Console.WriteLine("{0}, afina su instrumento",nombre);
+        }
+
+    }
+
+    class bajista:Musico
+    {
+        private string bajo;
+
+        public bajista(string no, string bajo):base(no)
+        {
+            this.bajo=bajo;
+        }
+
+        public void
+
+
+
+
+
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
