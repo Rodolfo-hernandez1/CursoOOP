2d71c46d4793cd1af59ce2c1789bf9f0c6565084
Rodolfo Hernandez <rodolfoh94@gmail.com>
2019-10-15 18:38:26 -0700

    EjemploDeclaseHoy

--- Programas/Herencia/EjemploHerencia/Program.cs
+++ Programas/Herencia/EjemploHerencia/Program.cs
@@ -1,64 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
-
-namespace EjemploHerencia
+namespace Musico
 {
-    class Musico
-    {
-        protected string nombre;
 
+    
+    class Musico 
+    {
+
+        protected string nombre;
         public Musico(string n)
         {
-            nombre = n;
+        nombre=n;
         }
 
-        public void saluda()
+        public void Saludo()
         {
-            Console.WriteLine("Hola Soy {0}",nombre);
+        Console.WriteLine("Hola,soy {0}",nombre);
         }
 
-        public void afinar()
+        public virtual void Afina() // se ponde virtual ya que con la lista bajista no saludaba como debe
         {
-            Console.WriteLine("{0}, afina su instrumento",nombre);
+            Console.WriteLine("{0},Afina su instrumento ",nombre);
         }
 
     }
 
-    class bajista:Musico
+    class Bajista:Musico
     {
-        private string bajo;
+       private string bajo;
+       public Bajista(string no,string b):base(no)  //se utiliza this en caso de ambiguedad si hubieran dos variables con el mismo nombre
+       {
+       bajo=b;
+       } 
 
-        public bajista(string no, string bajo):base(no)
-        {
-            this.bajo=bajo;
-        }
-
-        public void Afina()
-        {
-            Console.WriteLine("{0} Afinando su bajo {1}",nombre,bajo);
-        }
-
-
-
-
+       public override void  Afina() //tenia new pero se cambia a override ya que arriba se puso virtual , para que no se ate al metodo correspondiente atado dinamico
+       {
+           Console.WriteLine("{0} esta afinando su bajo {1}",nombre,bajo);
+       }
 
     }
 
+    class Guitarriasta:Musico
+    {   
+        private string Guitarra;
+        public Guitarrista(string gui,string g):base(gui)
+        {
+            Guitarra=g;
+        }
+
+        public override void Afina()
+        {
+            Console.WriteLine("{0}, esta afinando su Guitarra {1}",nombre,Guitarra);
+        }
 
 
-
+    }
     class Program
-    {
-        static void Main(string[] args)
+    { 
+
+        static void Main()
         {
-            Musico tom = new Musico("Tom");
-            bajista flea = new bajista("Flea","Gibson");
+            Musico tom=new Musico("Tom");
+            Bajista flea=new Bajista("Flea","MusicMan");
+            Guitarrista jason = new Guitarrista("Jason","Storm");
+           // tom.Saludo();
+           // flea.Saludo();
 
-            tom.saluda();
-            flea.saluda();
-            tom.afinar();
-            flea.Afina();
+          //  tom.Afina();
+          //  flea.Afina();
 
+            List <Musico> grupo = new List <Musico>();
+            grupo.Add(tom);
+            grupo.Add(flea);
+            grupo.Add(jason);
+            foreach(Musico m in grupo)
+                {
+                    m.Saludo();
+                    m.Afina();
+                }
         }
     }
 }
